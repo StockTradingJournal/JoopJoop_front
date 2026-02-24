0ddaea87ff11590f2c0a53b09c257239f06266c1
gyeongwon <skw4223@naver.com>
2026-02-24 22:45:46 +0900

    백엔드 url 변경 2

--- src/lib/gameSocket.ts
+++ src/lib/gameSocket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client';
 import { GameState, ItemType } from './socket-types';
 
-const BACKEND_URL = 'https://joopjoop-backend.onrender.com:8000/';
+const BACKEND_URL = 'https://joopjoop-backend.onrender.com/';
 
 class GameSocket {
   private socket: Socket | null = null;
